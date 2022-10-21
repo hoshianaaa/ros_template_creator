9547f47a9d604fcf6c88859a066d841e6509c188
hoshianaaa <ryotaro.hoshina@gmail.com>
2022-10-21 16:07:44 +0900

    Update

--- python_node.py
+++ python_node.py
@@ -13,7 +13,7 @@ else:
   node_name = arg1
   path = arg1 + ".py"
 
-string1 = '#!/usr/bin/env python\n\nimport rospy\nfrom std_msgs.msg import *\n\ndef callback(msg):\n  print(msg.data)\n\nrospy.init_node("'
+string1 = '#!/usr/bin/env python\n\nimport rospy\nfrom std_msgs.msg import *\nfrom geometry_msgs.msg import *\n\ndef callback(msg):\n  print(msg.data)\n\nrospy.init_node("'
 
 string2 = '")\npub = rospy.Publisher("~data", Float64, queue_size=10)\nrospy.Subscriber("~data", Float64, callback)\n\nr = rospy.Rate(10)\n\nwhile not rospy.is_shutdown():\n  pub.publish(Float64(0))\n  r.sleep()'
 
