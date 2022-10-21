e20cef2c4125baacc5f5c4940f2cf5f32c74c4a6
hoshianaaa <ryotaro.hoshina@gmail.com>
2022-10-21 16:02:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# ros_template_creator
+## python_node.py
+
+```
+python python_node.py xyz
+```
+
+output following file
+
+```python:xyz.py
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import *
+
+def callback(msg):
+  print(msg.data)
+
+rospy.init_node("xyz")
+pub = rospy.Publisher("~data", Float64, queue_size=10)
+rospy.Subscriber("~data", Float64, callback)
+
+r = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+  pub.publish(Float64(0))
+  r.sleep()
+```
---
+++ python_node.py
@@ -0,0 +1,23 @@
+import sys
+args = sys.argv
+arg1 = args[1]
+
+path = None
+node_name = None
+
+if (arg1 in "."):
+  split_string = arg1.split(".")
+  node_name = split_string[0]
+  path = arg1
+else:
+  node_name = arg1
+  path = arg1 + ".py"
+
+string1 = '#!/usr/bin/env python\n\nimport rospy\nfrom std_msgs.msg import *\n\ndef callback(msg):\n  print(msg.data)\n\nrospy.init_node("'
+
+string2 = '")\npub = rospy.Publisher("~data", Float64, queue_size=10)\nrospy.Subscriber("~data", Float64, callback)\n\nr = rospy.Rate(10)\n\nwhile not rospy.is_shutdown():\n  pub.publish(Float64(0))\n  r.sleep()'
+
+string = string1 + node_name + string2
+
+with open(path, 'a') as f:
+  print(string, file=f)
