72372bffb4fbbd61a6095460685a5273d7ac64b7
hoshianaaa <ryotaro.hoshina@gmail.com>
2022-10-21 17:03:55 +0900

    Update

--- launch_template.py
+++ launch_template.py
@@ -1,4 +1,6 @@
 import sys
+import os
+
 args = sys.argv
 arg1 = args[1]
 
@@ -14,7 +16,7 @@ else:
   node_name = arg1
   path = arg1 + extension
 
-data = open("~/ros_template_creator/templates/template.launch", "r")
+data = open( os.environ['HOME'] + "/ros_template_creator/templates/template.launch", "r")
 string = data.read()
 
 with open(path, 'a') as f:
